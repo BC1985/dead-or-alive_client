61f28d325c101c6185d1c6a3cd3ddd3313219084
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 15:32:45 -0400

    Make class component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Form.css";
 import Button from "../Button/Button";
 
-function Form() {
-  return (
-    <div className="form">
-      <input type="text" name="name-input" id="name-input" />
-      <Button>Search</Button>
-    </div>
-  );
+class Form extends Component {
+  render() {
+    return (
+      <div className="form">
+        <input type="text" name="name-input" id="name-input" />
+        <Button>Search</Button>
+      </div>
+    );
+  }
 }
 
 export default Form;
