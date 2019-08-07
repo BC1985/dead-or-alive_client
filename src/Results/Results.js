d0dda844c7773962ae3b75adfddd763ac795534c
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 13:53:12 -0400

    Add graphic

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -16,10 +16,15 @@ class Results extends Component {
               className="portrait"
               alt="portrait"
             />
-            <div className="name">Walt Whitman</div>
+            <div className="name">Walt Whitman is...</div>
           </div>
           <div className="status">
             <h1>Pushing up the daisies</h1>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjGlKmax4UKVFJc5JYFBOWC5Q6vgrbq8WSwcOnX-b2vwOQDT7l"
+              alt="headstone"
+              style={{ width: "50%" }}
+            />
             <p className="bio_dates">May 31, 1819 - March 26,1892</p>
           </div>
           <Form />
