1705655f81498e9e5fa9d534eefae9e00baa340f
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 16:41:53 -0400

    Rename components and import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Results from "./Results/Results-dead";
+import Results_Dead from "./Results/Results-dead";
+import Results_Alive from "./Results/Results-alive";
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <Router>
           <Switch>
             <LandingPage exact path="/" component={LandingPage} />
-            <Results path="/results-dead" component={Results} />
+            <Results_Dead path="/results-dead" component={Results_Dead} />
+            <Results_Alive path="/results-alive" component={Results_Dead} />
           </Switch>
         </Router>
       </div>
