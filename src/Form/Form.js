bc09b8203b5a4dd9f91b87ef5b048180b6e9e92d
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 16:42:16 -0400

    Create conditional results renering

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./Form.css";
 import Button from "../Button/Button";
+import { Link } from "react-router-dom";
 
 class Form extends Component {
+  state = {};
+
   render() {
     return (
       <div className="form">
         <input type="text" name="name-input" id="name-input" />
-        <Button>Search</Button>
+        {!this.props.alive ? (
+          <Link to="/results-dead">
+            <Button>Search</Button>
+          </Link>
+        ) : (
+          <Link to="/results-alive">
+            <Button>Search</Button>
+          </Link>
+        )}
       </div>
     );
   }
