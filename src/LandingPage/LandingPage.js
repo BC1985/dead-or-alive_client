c736eb03672e4f8662703ec9ca3b01be826eee67
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 15:32:30 -0400

    Add checkbox and make class component

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./LandingPage.css";
 import Form from "../Form/Form";
 
-function LandingPage() {
-  return (
-    <div className="landing-page-container">
-      <div className="header">
-        <h1>Dead or Alive</h1>
-        <p>
-          Enter name of famous person below to see if they are still alive or
-          dead
-        </p>
+class LandingPage extends Component {
+  state = {
+    alive: true
+  };
+  changeState = () => {
+    this.setState({
+      alive: false
+    });
+  };
+  render() {
+    return (
+      <div className="landing-page-container">
+        <div className="header">
+          <h1>Dead or Alive</h1>
+          <p>
+            Enter name of famous person below to see if they are still alive or
+            dead.
+          </p>
+        </div>
+        Check to return dead person
+        <input type="checkbox" name="checkbox" onClick={this.changeState} />
+        <div className="form">
+          <Form changeState={this.changeState} />
+        </div>
       </div>
-      <div className="form">
-        <Form />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default LandingPage;
