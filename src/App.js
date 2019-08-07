ee0a3f9a494b9dfbd1ab1e8c53b3825d0a427dcd
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 11:40:27 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Results from "./Results/Results";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <div className="App">
-          <LandingPage />
-        </div>
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <Switch>
+          <LandingPage exact path="/" component={LandingPage} />
+          <Results path="/results" component={Results} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
