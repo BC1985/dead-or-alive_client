a2e493fcebfface0ddf7e5bd5d79547ae8844340
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 16:27:00 -0400

    Make class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Results from "./Results/Results-dead";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <LandingPage exact path="/" component={LandingPage} />
-          <Results path="/results-dead" component={Results} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <LandingPage exact path="/" component={LandingPage} />
+            <Results path="/results-dead" component={Results} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
