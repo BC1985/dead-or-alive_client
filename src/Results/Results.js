c7e637f22abdbb21069b0562b88bcb952adab1a4
Ben Cohen <benjcohen1985@gmail.com>
2019-08-07 11:41:11 -0400

    Add results page

---
+++ src/Results/Results.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div className="results-container">
+          <img src="" alt="portrait" />
+          <h1>Dead</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Results;
