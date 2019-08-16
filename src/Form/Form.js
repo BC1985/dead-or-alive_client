847eb310fcf527c24e57b8a61c68717dd0c91584
Ben Cohen <benjcohen1985@gmail.com>
2019-08-15 20:59:48 -0400

    Temporarily disable form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
-import "./Form.css";
-import Button from "../Button/Button";
-import { Link } from "react-router-dom";
+// import React, { Component } from "react";
+// import "./Form.css";
+// // import Button from "../Button/Button";
+// import { Link } from "react-router-dom";
+// import { withRouter } from "react-router-dom";
 
-class Form extends Component {
-  state = {};
+// class Form extends Component {
+//   render() {
+//     return (
+//       <form className="form">
+//         <input
+//           type="text"
+//           name="person_name"
+//           id="name"
+//           onChange={this.changeHandler}
+//         />
+//         <Link to="/">
+//           <button type="submit" onClick={this.handleSubmit}>
+//             Search
+//           </button>
+//         </Link>
+//         {this.state.submitted && "Submitted"}
+//       </form>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <div className="form">
-        <input type="text" name="name-input" id="name-input" />
-        {this.props.checkboxState ? (
-          <Link to="/results-dead">
-            <Button>Search</Button>
-          </Link>
-        ) : (
-          <Link to="/results-alive">
-            <Button>Search</Button>
-          </Link>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Form;
+// export default withRouter(Form);
