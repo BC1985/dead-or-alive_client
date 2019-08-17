6175e35474df607a38467d034de4207c21becdd2
Ben Cohen <benjcohen1985@gmail.com>
2019-08-17 15:46:31 -0400

    Pass props to parent component

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,48 +1,14 @@
 import React, { Component } from "react";
 import "./LandingPage.css";
 // import Results from "../Results/Results";
-import config from "../config";
+import { withRouter } from "react-router-dom";
 
 class LandingPage extends Component {
-  state = {
-    people: [],
-    person: "",
-    result: []
-  };
   handleSubmit = e => {
     e.preventDefault();
-    this.filterPeople();
+    this.props.filterPeople();
   };
-  componentDidMount() {
-    this.fetchPeople();
-  }
-  fetchPeople = async () => {
-    try {
-      const data = await fetch(`${config.API_ENDPOINT}/people/`);
-      const people = await data.json();
-      this.setState({
-        people
-      });
-      console.log(this.state.people);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  filterPeople = () => {
-    const filteredPerson = this.state.people.filter(person => {
-      return person.person_name === this.state.person;
-    });
-    const person = filteredPerson[0].person_name;
-    this.state.result.push(person);
-    console.log(this.state.result);
 
-    this.props.history.push("/results");
-  };
-  changeHandler = e => {
-    this.setState({
-      person: e.target.value
-    });
-  };
   render() {
     return (
       <div className="landing-page-container">
@@ -58,8 +24,8 @@ class LandingPage extends Component {
             type="text"
             name="person_name"
             id="name"
-            value={this.state.person}
-            onChange={this.changeHandler}
+            value={this.props.person}
+            onChange={this.props.changeHandler}
           />
           <button type="submit" onClick={this.handleSubmit}>
             Search
@@ -70,4 +36,4 @@ class LandingPage extends Component {
   }
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
