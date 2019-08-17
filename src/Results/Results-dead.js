e240bbde00f4f21fa7b56a64f1fc6479934ef105
Ben Cohen <benjcohen1985@gmail.com>
2019-08-17 14:23:22 -0400

    Temporarily disable component

--- src/Results/Results-dead.js
+++ src/Results/Results-dead.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
-import "./Results.css";
-class Results extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <div className="results-container">
-          <div className="image-container">
-            <img
-              src="https://daily.jstor.org/wp-content/uploads/2019/04/should_walt_whitman_be_cancelled_1050x700.jpg"
-              className="portrait"
-              alt="portrait"
-            />
-            <div className="name">Walt Whitman is...</div>
-          </div>
-          <div className="status">
-            <h1>Pushing up the daisies</h1>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjGlKmax4UKVFJc5JYFBOWC5Q6vgrbq8WSwcOnX-b2vwOQDT7l"
-              alt="headstone"
-              style={{ width: "50%" }}
-            />
-            <p className="bio_dates">May 31, 1819 - March 26,1892</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// import React, { Component } from "react";
+// import "./Results.css";
+// class Results extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <div className="results-container">
+//           <div className="image-container">
+//             <img
+//               src="https://daily.jstor.org/wp-content/uploads/2019/04/should_walt_whitman_be_cancelled_1050x700.jpg"
+//               className="portrait"
+//               alt="portrait"
+//             />
+//             <div className="name">Walt Whitman is...</div>
+//           </div>
+//           <div className="status">
+//             <h1>Pushing up the daisies</h1>
+//             <img
+//               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjGlKmax4UKVFJc5JYFBOWC5Q6vgrbq8WSwcOnX-b2vwOQDT7l"
+//               alt="headstone"
+//               style={{ width: "50%" }}
+//             />
+//             <p className="bio_dates">May 31, 1819 - March 26,1892</p>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Results;
+// export default Results;
