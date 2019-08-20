4ecf23e7eb4976f2d299c6ecfde2478e52c48d9d
Ben Cohen <benjcohen1985@gmail.com>
2019-08-20 07:53:28 -0400

    Add 404 screen

---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { generateRandomFact } from "../Utils/randomFact";
+import { NavLink } from "react-router-dom";
+import error from "./error.png";
+import "./NotFound.css";
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="not-found-container">
+        <h1>Uh oh...</h1>
+        <div className="error-graphic">
+          <img src={error} alt="404 error" id="error-graphic" />
+        </div>
+        <p style={{ marginBottom: "50px", textAlign: "left" }}>
+          The person you requested is unavailable. The name has been submitted,
+          and their information will be uploaded soon. In the meantime, here is
+          an interesting music fact... {generateRandomFact()}
+        </p>
+        <NavLink to="/">
+          <button>Back</button>
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
