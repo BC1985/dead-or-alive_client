2a56fd86b89fb46878df77c26b6d51025afceb97
Ben Cohen <benjcohen1985@gmail.com>
2019-08-23 10:50:48 -0400

    Remove function

--- src/Utils/capitalizeName.js
+++
@@ -1,9 +0,0 @@
-export function capitalizeName(str) {
-  str = str.split(" ");
-
-  for (var i = 0, x = str.length; i < x; i++) {
-    str[i] = str[i][0].toUpperCase() + str[i].substr(1);
-  }
-
-  return str.join(" ");
-}
